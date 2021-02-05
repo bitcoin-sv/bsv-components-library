54bc89a2da3911bea37139755c7ba7b4cb8e91cd
ilozanof <lozanofernandez@yahoo.es>
2021-02-05 14:40:52 +0100

    Removing the "base" module in JCL (we are using "bitcoinJ" and "tools" instead)

--- base/src/main/java/com/nchain/jcl/base/tools/handlers/HandlerConfig.java
+++
@@ -1,9 +0,0 @@
-package com.nchain.jcl.base.tools.handlers;
-
-/**
- * @author i.fernandez@nchain.com
- * Copyright (c) 2018-2020 nChain Ltd
- *
- * A base class for a Handler Configuration Class.
- */
-public class HandlerConfig {}
