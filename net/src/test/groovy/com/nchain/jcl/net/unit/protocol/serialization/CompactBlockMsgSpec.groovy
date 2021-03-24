a62b556f4c7d7f33b5517d091399e1f49c6978da
Jan Pomer <jan.pomer@equaleyes.com>
2021-03-24 10:16:34 +0100

    implement compact block serialization test

--- net/src/test/groovy/com/nchain/jcl/net/unit/protocol/serialization/CompactBlockMsgSpec.groovy
+++ net/src/test/groovy/com/nchain/jcl/net/unit/protocol/serialization/CompactBlockMsgSpec.groovy
@@ -2,11 +2,15 @@ package com.nchain.jcl.net.unit.protocol.serialization
 
 import com.nchain.jcl.net.protocol.config.ProtocolConfig
 import com.nchain.jcl.net.protocol.config.ProtocolConfigBuilder
-import com.nchain.jcl.net.protocol.messages.CompactBlockMsg
+import com.nchain.jcl.net.protocol.messages.*
 import com.nchain.jcl.net.protocol.serialization.CompactBlockMsgSerializer
+import com.nchain.jcl.net.protocol.serialization.TxInputMsgSerializer
+import com.nchain.jcl.net.protocol.serialization.TxOutputMsgSerializer
 import com.nchain.jcl.net.protocol.serialization.common.DeserializerContext
+import com.nchain.jcl.net.protocol.serialization.common.SerializerContext
 import com.nchain.jcl.net.unit.protocol.tools.ByteArrayArtificalStreamProducer
 import com.nchain.jcl.tools.bytes.ByteArrayReader
+import com.nchain.jcl.tools.bytes.ByteArrayWriter
 import io.bitcoinj.core.Utils
 import io.bitcoinj.params.MainNetParams
 import io.bitcoinj.params.Net
@@ -31,7 +35,7 @@ class CompactBlockMsgSpec extends Specification {
 
     private static final String COMPACT_BLOCK_BYTES = BLOCK_HEADER_BYTES + NONCE + NUMBER_OF_SHORT_IDS_BYTES + SHORT_TX_IDS_BYTES + NUMBER_OF_PREFILLED_TX_BYTES
 
-    def "Testing PrefilledTransactionMsg Deserialize"(int byteInterval, int delayMs) {
+    def "Testing CompactBlockMsg Deserialize"(int byteInterval, int delayMs) {
         given:
             ProtocolConfig config = ProtocolConfigBuilder.get(new MainNetParams(Net.MAINNET))
             DeserializerContext context = DeserializerContext.builder()
@@ -58,5 +62,47 @@ class CompactBlockMsgSpec extends Specification {
         where:
             byteInterval | delayMs
             10           | 5
+    }
+
+    def "Testing CompactBlockMsg Serializing"() {
+        given:
+            ProtocolConfig config = ProtocolConfigBuilder.get(new MainNetParams(Net.MAINNET))
+            SerializerContext context = SerializerContext.builder()
+                .protocolBasicConfig(config.getBasicConfig())
+                .build()
+
+            CompactBlockMsgSerializer serializer = CompactBlockMsgSerializer.getInstance()
+
+            BasicBlockHeaderMsg blockHeaderMsg = BasicBlockHeaderMsg.builder()
+                .hash(HashMsg.builder().hash(Utils.HEX.decode("2f6428543c10f8b30e76f9519b597259e4839e0c91ed278a7eee27f8c014a525")).build())
+                .version(1)
+                .prevBlockHash(HashMsg.builder().hash(Utils.HEX.decode("40f11b68435988807d64dff20261f7d9827825fbb37542601fb94d4500000000")).build())
+                .merkleRoot(HashMsg.builder().hash(Utils.HEX.decode("5d0a2717cccfb28565e04baf2708f32068fb80f98765210ce6247b8939ab2012")).build())
+                .creationTimestamp(1288886764)
+                .difficultyTarget(486622232)
+                .nonce(90297088)
+                .build()
+
+            long[] shortTxIds = new long[3]
+            shortTxIds[0] = 72843215973449;
+            shortTxIds[1] = 178945551052361;
+            shortTxIds[2] = 55525874428521;
+
+            CompactBlockMsg compactBlockMsg = CompactBlockMsg.builder()
+                .header(blockHeaderMsg)
+                .nonce(5921250825923725)
+                .shortTxIds(shortTxIds)
+                .prefilledTransactions(new PrefilledTxMsg[0])
+                .build()
+
+            String messageSerializedBytes
+        when:
+            ByteArrayWriter byteWriter = new ByteArrayWriter()
+            serializer.serialize(context, compactBlockMsg, byteWriter)
+            messageSerializedBytes = Utils.HEX.encode(byteWriter.reader().getFullContent())
+            byteWriter.reader()
+
+        then:
+            messageSerializedBytes == COMPACT_BLOCK_BYTES
     }
 }
