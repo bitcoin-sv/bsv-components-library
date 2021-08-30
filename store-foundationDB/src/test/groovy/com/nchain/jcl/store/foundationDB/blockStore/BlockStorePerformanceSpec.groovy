cbf55f58a857b0a84cc8370d075d09877d548445
Ivan J Lozano Fernandez <i.fernandez@nchain.com>
2021-08-30 13:31:07 +0200

    Disabling FDB Tests

--- store-foundationDB/src/test/groovy/com/nchain/jcl/store/foundationDB/blockStore/BlockStorePerformanceSpec.groovy
+++ store-foundationDB/src/test/groovy/com/nchain/jcl/store/foundationDB/blockStore/BlockStorePerformanceSpec.groovy
@@ -5,11 +5,15 @@ import com.nchain.jcl.store.blockStore.BlockStorePerformanceSpecBase
 import com.nchain.jcl.store.blockStore.metadata.Metadata
 import com.nchain.jcl.store.foundationDB.FDBTestUtils
 import com.nchain.jcl.store.foundationDB.StoreFactory
+import spock.lang.Ignore
 
 
 /**
  * Performance Testing class for the BlockStore
  */
+// Test Ignored. If you want to run this Test, set up a local FDB or configure FDBTestUtils.useDocker to use the
+// Docker image provided instead (not fully tested at the moment)
+@Ignore
 class BlockStorePerformanceSpec extends BlockStorePerformanceSpecBase {
 
     // Start & Stop FoundationDB in Docker Container (check DockerTestUtils for details)...
