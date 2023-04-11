c67993e8643f877ecc30eb55f3e9aee7cd74fd08
Daniel Connolly <d.connolly@nchain.com>
2023-04-11 13:17:41 +0200

    Replace sleeps with waiting on events in a test (#19)
    * replace some of the sleeps with waiting on events
    
    * add awaitStarted() method to P2P
    remove more sleeps in tests
    
    * check that timeouts didnt happen
    
    * update sonarqube plugin to handle gradle 8

--- net/src/test/groovy/io/bitcoinsv/bsvcl/net/unit/protocol/handlers/wrapper/ProtocolMsgsTest.groovy
+++ net/src/test/groovy/io/bitcoinsv/bsvcl/net/unit/protocol/handlers/wrapper/ProtocolMsgsTest.groovy
@@ -13,7 +13,9 @@ import io.bitcoinsv.bsvcl.net.protocol.wrapper.P2PBuilder
 import io.bitcoinsv.bitcoinjsv.params.MainNetParams
 import spock.lang.Specification
 
+import java.util.concurrent.CountDownLatch
 import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.TimeUnit
 
 /**
  * Testing class using the P2P "wrapper", that includes all the network and protocol handlers within.
@@ -51,6 +53,9 @@ class ProtocolMsgsTest extends Specification {
             AtomicInteger numConnections = new AtomicInteger()
             AtomicInteger numDisconnections = new AtomicInteger()
             AtomicInteger numMsgs = new AtomicInteger()
+            CountDownLatch rdyLatch = new CountDownLatch(2)
+            CountDownLatch disconnectedLatch = new CountDownLatch(2)
+            CountDownLatch msgsRecv = new CountDownLatch(NUM_MSGS)
 
             server.EVENTS.PEERS.CONNECTED.forEach({ e ->
                 println("> Server :: peer connected: " + e.peerAddress)
@@ -60,57 +65,63 @@ class ProtocolMsgsTest extends Specification {
             })
             server.EVENTS.PEERS.HANDSHAKED.forEach({ e ->
                 numConnections.incrementAndGet()
+                rdyLatch.countDown()
                 println("> Server :: peer handshaked: " + e.peerAddress)
             })
             client.EVENTS.PEERS.HANDSHAKED.forEach({ e ->
                 numConnections.incrementAndGet()
+                rdyLatch.countDown()
                 println("> Client :: peer handshaked: " + e.peerAddress)
             })
             server.EVENTS.PEERS.DISCONNECTED.forEach({ e ->
                 numDisconnections.incrementAndGet()
+                disconnectedLatch.countDown()
                 println("> Server :: peer disconnected: " + e.peerAddress)
             })
             client.EVENTS.PEERS.DISCONNECTED.forEach({ e ->
                 numDisconnections.incrementAndGet()
+                disconnectedLatch.countDown()
                 println("> Client :: peer disconnected: " + e.peerAddress)
             })
             server.EVENTS.MSGS.ADDR.forEach({ e ->
                 numMsgs.incrementAndGet()
+                msgsRecv.countDown()
+                println("> Server :: ADDR MSG received")
             })
 
         when:
             server.startServer()
             client.start()
+            server.awaitStarted(5, TimeUnit.SECONDS)
+            client.awaitStarted(5, TimeUnit.SECONDS)
 
-            Thread.sleep(100)
             client.REQUESTS.PEERS.connect(server.getPeerAddress()).submit()
 
             // We wait until the Handshake is done
-            while (numConnections.get() < 2) {
-                Thread.sleep(10)
-            }
+            boolean hndshakeDone = rdyLatch.await(5, TimeUnit.SECONDS)
 
             // We send a few messages from the Client to the Server:
-            BitcoinMsg<AddrMsg> msg = MsgTest.getAddrMsg();
+            BitcoinMsg<AddrMsg> msg = MsgTest.getAddrMsg()
             for (int i = 0; i < NUM_MSGS; i++) {
                 println(" >> SENDING ADDR MSG...")
                 client.REQUESTS.MSGS.send(server.getPeerAddress(), msg).submit()
             }
 
-            // NOTE: Here, we wait a little bit before we close the connection If we don't wait enough, the Socket
-            // between the Client and Server will be closed before the message can be serialized/Deserialized and
-            // travel between them
-            Thread.sleep(100)
+            boolean msgsReceived = msgsRecv.await(30, TimeUnit.SECONDS)
+
             println(" >>> DISCONNECTING FROM THE SERVER...")
             client.REQUESTS.PEERS.disconnect(server.getPeerAddress()).submit()
+            boolean disconDone = disconnectedLatch.await(5, TimeUnit.SECONDS)
 
-            Thread.sleep(100)
             println(" >>> STOPPING...")
             server.stop()
             client.stop()
 
         then:
             // We check that the Events have been triggered right:
+            hndshakeDone
+            msgsReceived
+            disconDone
             numConnections.get() == 2
             numDisconnections.get() >= 2
             numMsgs.get() == NUM_MSGS
